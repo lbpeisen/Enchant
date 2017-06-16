04f787ee11d5e2e81e0899f726a862bacab4a026
rain <han784851198@live.com>
2017-06-16 16:29:16 +0800

    添加评论界面完的回复，此版本可以使用。

--- app/src/main/java/me/wcy/music/fragment/SwipeFragment.java
+++ app/src/main/java/me/wcy/music/fragment/SwipeFragment.java
@@ -1,21 +1,46 @@
 package me.wcy.music.fragment;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.labo.kaji.swipeawaydialog.SwipeAwayDialogFragment;
 
 import me.wcy.music.R;
 
 public class SwipeFragment extends SwipeAwayDialogFragment {
+
+    private EditText editText;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("回复");
+        editText = new EditText(getActivity());
+        editText.setFocusable(true);
+        editText.setFocusableInTouchMode(true);
+        editText.requestFocus();
+        builder.setView(editText);
+        builder.setPositiveButton(android.R.string.ok, new Dialog.OnClickListener() {
+            /*
+            * 回复操作
+            * */
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String comment = editText.getText().toString();
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton(android.R.string.cancel, new Dialog.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        return builder.create();
     }
 }
